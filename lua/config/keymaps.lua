0394372b1747356db744b71a5e7a357153795f29
lcw <lcw.pub@foxmail>
2025-12-07 16:26:46 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,10 +29,10 @@ map("n", "n", "nzz", { noremap = true, silent = true })
 map("n", "<c-u>", "<c-r>", { noremap = false, silent = true })
 
 -- 分屏切换（leader + 方向）
-map("n", "<leader>k", "<c-w>k", { noremap = false, silent = true })
-map("n", "<leader>j", "<c-w>j", { noremap = false, silent = true })
-map("n", "<leader>h", "<c-w>h", { noremap = false, silent = true })
-map("n", "<leader>l", "<c-w>l", { noremap = false, silent = true })
+map("n", "<a-k>", "<c-w>k", { noremap = false, silent = true })
+map("n", "<a-j>", "<c-w>j", { noremap = false, silent = true })
+map("n", "<a-h>", "<c-w>h", { noremap = false, silent = true })
+map("n", "<a-l>", "<c-w>l", { noremap = false, silent = true })
 
 -- 调整分屏大小（方向键）
 map("n", "<up>", ":res -5<cr>", { noremap = false, silent = true })
@@ -57,6 +57,9 @@ map("v", "<leader>nm", ":norm ", { noremap = false, silent = false })
 map("v", ">", ">gv", { noremap = true, silent = true })
 map("v", "<", "<gv", { noremap = true, silent = true })
 
+-- 切换高亮
+map("n", "<leader>hl", ":set hlsearch!<cr>", { noremap = true, silent = true })
+
 
 -- #BEGIN Plugin Keymaps
 
@@ -64,8 +67,8 @@ map("v", "<", "<gv", { noremap = true, silent = true })
 map("n", "<a-m>", ":NvimTreeOpen<cr>", { noremap = false, silent = false })
 
 -- bufferline 左右Tab切换
-map("n", "<a-h>", ":BufferLineCyclePrev<cr>", { noremap = false, silent = false })
-map("n", "<a-l>", ":BufferLineCycleNext<cr>", { noremap = false, silent = false })
+map("n", "<a-p>", ":BufferLineCyclePrev<cr>", { noremap = false, silent = false })
+map("n", "<a-n>", ":BufferLineCycleNext<cr>", { noremap = false, silent = false })
 -- 新建buffer
 map("n", "<leader>bn", ":enew <cr>", { noremap = true, silent = true })
 -- 关闭当前buffer
@@ -82,40 +85,44 @@ map("n", "<leader>bl", ":BufferLineCloseLeft<cr>", { noremap = true, silent = tr
 -- 终端配置
 -- 支持esc退出插入模式
 -- map("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
-map("t", "<leader>l", "<Cmd> wincmd l<CR>", { noremap = true, silent = true })
-map("t", "<leader>h", "<Cmd> wincmd h<CR>", { noremap = true, silent = true })
-map("t", "<leader>j", "<Cmd> wincmd j<CR>", { noremap = true, silent = true })
-map("t", "<leader>k", "<Cmd> wincmd k<CR>", { noremap = true, silent = true })
+map("t", "<a-l>", "<Cmd> wincmd l<cr>", { noremap = true, silent = true })
+map("t", "<a-h>", "<Cmd> wincmd h<cr>", { noremap = true, silent = true })
+map("t", "<a-j>", "<Cmd> wincmd j<cr>", { noremap = true, silent = true })
+map("t", "<a-k>", "<Cmd> wincmd k<cr>", { noremap = true, silent = true })
 
 -- treesitter-textobjects 配置
 -- 选中「函数外部」（包含函数名 / 括号等）
-vim.keymap.set({ "x", "o" }, "af", function()
+map({ "x", "o" }, "af", function()
   require "@function.outer".select_textobject("nvim-treesitter-textobjects.select", "textobjects")
 end, { desc = "选中「函数外部」" })
 -- 选中「函数内部」（仅函数体）
-vim.keymap.set({ "x", "o" }, "if", function()
+map({ "x", "o" }, "if", function()
   require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
 end, { desc = "选中「函数内部」" })
 -- 选中「类外部」（包含类定义边界）
-vim.keymap.set({ "x", "o" }, "ac", function()
+map({ "x", "o" }, "ac", function()
   require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
 end, { desc = "选中「类外部」" })
 -- 选中「类内部」（仅类体）
-vim.keymap.set({ "x", "o" }, "ic", function()
+map({ "x", "o" }, "ic", function()
   require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
 end, { desc = "选中「类内部」" })
 -- 选中「局部作用域」（比如函数内的作用域）
-vim.keymap.set({ "x", "o" }, "as", function()
+map({ "x", "o" }, "as", function()
   require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")
 end, { desc = "选中「局部作用域」" })
 -- 将当前参数与下一个参数替换
-vim.keymap.set("n", "<leader>a", function()
+map("n", "<leader>a", function()
   require("nvim-treesitter-textobjects.swap").swap_next "@parameter.inner"
 end, { desc = "将当前参数与下一个参数替换" })
 -- 将当前参数与上一个参数替换
-vim.keymap.set("n", "<leader>A", function()
+map("n", "<leader>A", function()
   require("nvim-treesitter-textobjects.swap").swap_previous "@parameter.outer"
 end, { desc = "将当前参数与上一个参数替换" })
+
+-- Markdown
+map("n", "<leader>md", ":MarkdownPreviewToggle<cr>", { noremap = true, silent = true })
+map("n", "<leader>mds", ":MarkdownPreviewStop<cr>", { noremap = true, silent = true })
 
 -- #END Plugin Keymaps
 
