3a004be4bd9c3b6e9f092e6f69b9107f4305dc8b
lcw <lcw.pub@foxmail.com>
2026-05-06 15:54:48 +0800

    update

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,31 +1,44 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    on_attach = function ()
-      -- 跳转到声明
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.declaration()<CR>", {silent = true, noremap = true})
-      -- 跳转到定义
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>", {silent = true, noremap = true})
-      -- 显示注释文档
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {silent = true, noremap = true})
-      -- 跳转到实现
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {silent = true, noremap = true})
-      -- 跳转到引用位置
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {silent = true, noremap = true})
-      -- 以浮窗形式显示错误
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", {silent = true, noremap = true})
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", {silent = true, noremap = true})
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "gn", "<cmd>lua vim.diagnostic.goto_next()<CR>", {silent = true, noremap = true})
-    end
+    config = function()
+      -- LSP 附加到 buffer 时自动设置快捷键
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(args)
+          local opts = { buffer = args.buf, silent = true }
+          -- 跳转到声明
+          vim.keymap.set("n", "gd", vim.lsp.buf.declaration, opts)
+          -- 跳转到定义
+          vim.keymap.set("n", "gD", vim.lsp.buf.definition, opts)
+          -- 显示悬浮文档
+          vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+          -- 跳转到实现
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          -- 跳转到引用
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          -- 以浮窗显示诊断
+          vim.keymap.set("n", "go", vim.diagnostic.open_float, opts)
+          vim.keymap.set("n", "gp", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "gn", vim.diagnostic.goto_next, opts)
+        end,
+      })
+    end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      -- ensure_installed = { "lua_ls", "rust_analyzer" },
+      -- 所有通过 Mason 安装的 LSP server 都会经过 handlers 自动 setup
+      handlers = {
+        -- 默认 handler：应用于所有 server
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end,
+      },
     },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
-    }
-  }
+    },
+  },
 }
